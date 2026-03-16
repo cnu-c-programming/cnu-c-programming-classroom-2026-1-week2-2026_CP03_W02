227daeb97dd34ff4e7cbb1ec9409db2701230d73
ldh0906 <ldh0906@github.com>
2026-03-16 15:01:00 +0900

    4-3

--- code/4-3.c
+++ code/4-3.c
@@ -4,7 +4,10 @@ int main()
 {
     int input;
     scanf("%d", &input);
-    
+    if(input == 1) {
+        printf("false");
+        goto label;
+    }
     for (int i = 2; i < input; i++) {
         if (input % i == 0) {
             printf("false");
